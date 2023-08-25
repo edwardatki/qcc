4cc254380020a75792cb26105b96bf409f4a6650
Ed Atkinson <edwardatki@gmail.com>
2023-08-26 00:42:09 +0100

    Fix error position indication being wrong

--- src/lexer.c
+++ src/lexer.c
@@ -3,7 +3,7 @@
 
 static Token* curToken;
 
-int currentLine = 0;
+int currentLine = 1;
 int currentColumn = 0;
 
 static Token* newToken(enum TokenType type) {
@@ -12,13 +12,13 @@ static Token* newToken(enum TokenType type) {
     return token;
 }
 
-static void addToken(enum TokenType type, char* value) {
+static void addToken(enum TokenType type, char* value, int line, int column) {
     Token* token = newToken(T_END);
     curToken->next = token;
     curToken->type = type;
     curToken->value = value;
-    curToken->line = currentLine;
-    curToken->column = currentColumn;
+    curToken->line = line;
+    curToken->column = column;
     curToken = token;
 }
 
@@ -41,6 +41,8 @@ static int isWhitespace(char c) {
 }
 
 static void checkNumeric(FILE* fp, char c) {
+    int startColumn = currentColumn;
+
     char* value = calloc(32, sizeof(char));
     int i = 0;
     while(1) {
@@ -49,10 +51,12 @@ static void checkNumeric(FILE* fp, char c) {
         c = next(fp);
     }
 
-    addToken(T_NUMBER, value);
+    addToken(T_NUMBER, value, currentLine, startColumn);
 }
 
 static void checkKeyword(FILE* fp, char c) {
+    int startColumn = currentColumn;
+
     char* value = calloc(32, sizeof(char));
     int i = 0;
     while(1) {
@@ -64,15 +68,15 @@ static void checkKeyword(FILE* fp, char c) {
     // Check if a defined type
     for (int i = 0; i < sizeof(builtinTypes)/sizeof(builtinTypes[0]); i++) {
         if (strcmp(value, builtinTypes[i].name) == 0) {
-            addToken(T_TYPE, value);
+            addToken(T_TYPE, value, currentLine, startColumn);
             return;
         }
     }
 
-    if (strcmp(value, "return") == 0) addToken(T_RETURN, value);
-    else if (strcmp(value, "if") == 0) addToken(T_IF, value);
-    else if (strcmp(value, "else") == 0) addToken(T_ELSE, value);
-    else addToken(T_ID, value);
+    if (strcmp(value, "return") == 0) addToken(T_RETURN, value, currentLine, startColumn);
+    else if (strcmp(value, "if") == 0) addToken(T_IF, value, currentLine, startColumn);
+    else if (strcmp(value, "else") == 0) addToken(T_ELSE, value, currentLine, startColumn);
+    else addToken(T_ID, value, currentLine, startColumn);
 }
 
 Token* lex(FILE* fp) {
@@ -88,29 +92,30 @@ Token* lex(FILE* fp) {
         }
 
         if (c == EOF) {
-            addToken(T_END, NULL);
+            addToken(T_END, NULL, currentLine, currentColumn);
             break;
         }
 
-        if (isWhitespace(c)) continue;
-
-        if ((c == '/') && (peek(fp) == '/')) {
-            while (next(fp) != '\n') continue;
-            currentLine += 1;
+        if (isWhitespace(c)) {
             continue;
         }
 
-        if (c == '(') {addToken(T_LPAREN, "{"); continue;}
-        if (c == ')') {addToken(T_RPAREN, "}"); continue;}
-        if (c == '{') {addToken(T_LBRACE, "{"); continue;}
-        if (c == '}') {addToken(T_RBRACE, "}"); continue;}
-        if (c == ',') {addToken(T_COMMA, ","); continue;}
-        if (c == '+') {addToken(T_PLUS, "+"); continue;}
-        if (c == '-') {addToken(T_MINUS, "-"); continue;}
-        if (c == '*') {addToken(T_MUL, "*"); continue;}
-        if (c == '/') {addToken(T_DIV, "/"); continue;}
-        if (c == '=') {addToken(T_ASSIGN, "="); continue;}
-        if (c == ';') {addToken(T_SEMICOLON, ";"); continue;}
+        if ((c == '/') && (peek(fp) == '/')) {
+            while (peek(fp) != '\n') next(fp);
+            continue;
+        }
+
+        if (c == '(') {addToken(T_LPAREN, "{", currentLine, currentColumn); continue;}
+        if (c == ')') {addToken(T_RPAREN, "}", currentLine, currentColumn); continue;}
+        if (c == '{') {addToken(T_LBRACE, "{", currentLine, currentColumn); continue;}
+        if (c == '}') {addToken(T_RBRACE, "}", currentLine, currentColumn); continue;}
+        if (c == ',') {addToken(T_COMMA, ",", currentLine, currentColumn); continue;}
+        if (c == '+') {addToken(T_PLUS, "+", currentLine, currentColumn); continue;}
+        if (c == '-') {addToken(T_MINUS, "-", currentLine, currentColumn); continue;}
+        if (c == '*') {addToken(T_MUL, "*", currentLine, currentColumn); continue;}
+        if (c == '/') {addToken(T_DIV, "/", currentLine, currentColumn); continue;}
+        if (c == '=') {addToken(T_ASSIGN, "=", currentLine, currentColumn); continue;}
+        if (c == ';') {addToken(T_SEMICOLON, ";", currentLine, currentColumn); continue;}
 
         if (isdigit(c)) {checkNumeric(fp, c); continue;}
         if (isalpha(c)) {checkKeyword(fp, c); continue;}
